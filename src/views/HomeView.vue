95f3da7968f402c53b780a7fefdeaffdc7d65dea
dheeman pati <dheeman.p@simformsolutions.com>
2024-01-17 19:20:40 +0530

    feat: login and home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <MainLayout>
+    <template #main>
+      <div class="grid grid-rows-[4fr_12fr] gap-y-28">
+        <CreateDocument />
+        <div class="bg-white grid grid-cols-1 md:grid-cols-[30%_40%_30%] h-[100%]">
+          <div class="md:col-start-2 md:col-end-3 pt-4">
+            <div class="flex items-center justify-between">
+              <div class="flex gap-2">
+                <FolderIcon class="w-6 h-6" />
+                <p>My Documents</p>
+              </div>
+              <p>Date Created</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <template #header>
+      <Header />
+    </template>
+  </MainLayout>
+</template>
+<script setup lang="ts">
+import MainLayout from '@/layout/MainLayout.vue'
+import Header from '@/components/Header.vue'
+import CreateDocument from '@/components/CreateDocument.vue'
+import { FolderIcon } from '@heroicons/vue/24/solid'
+</script>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="h-[100vh] flex justify-center items-center">
+   <div class="flex flex-col items-center justify-center">
+    <img src="@/assets/docs.png" class="w-[40%] h-[40%]" alt="docs logo">
+    <button class="mt-3 text-white shadow-sm px-4 py-2 border-none rounded-lg bg-google-blue">Sign In</button>
+   </div>
+
+</div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>

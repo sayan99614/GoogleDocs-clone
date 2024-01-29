d3651d788efe24736945039d104a7ca1a3fbd7bb
dheeman pati <dheeman.p@simformsolutions.com>
2024-01-29 19:26:08 +0530

    feat: added firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBCiwIM6QBb7rS8SCp9gBVYPZGehwG_FGA',
+  authDomain: 'docs-clone-89bbd.firebaseapp.com',
+  projectId: 'docs-clone-89bbd',
+  storageBucket: 'docs-clone-89bbd.appspot.com',
+  messagingSenderId: '120832790091',
+  appId: '1:120832790091:web:f50a5f7084c7df9d31c7ce'
+}
+
+// Initialize Firebase
+export const firebaseApp:any= initializeApp(firebaseConfig)
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,26 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueFire, VueFireAuth } from 'vuefire'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-
+import { firebaseApp } from '@/firebaseConfig'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()]
+})
+
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')

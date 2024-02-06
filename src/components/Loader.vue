4a90c85e2efd7edde8c3e56bcef829d28a89776a
dheeman pati <dheeman.p@simformsolutions.com>
2024-02-06 18:27:29 +0530

    feat:loader in home screen

---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="flex items-center justify-center">
+    <div class="relative">
+      <div class="h-12 w-12 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+      <div
+        class="absolute top-0 left-0 h-12 w-12 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"
+      ></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
